c690369afba332372ec40eb16d18d7d74f6b18a9
guilherme <guilhermesemusa@gmail.com>
2020-11-25 00:30:45 -0300

    Desafio 1
    teste do desafio 1

---
+++ index.js
@@ -0,0 +1,16 @@
+//DESAFIO 1
+
+const idade = 17
+//verificar se a pessoa é maior ou igual a  18
+//Se sim, deixar entrar, senão, bloquear a entrada
+if (idade >= 18){
+    console.log(`Você têm ${idade} anos e pode entrar.`)
+}else{
+    console.log('Você não pode entrar,pois é menor de idade.')
+}
+//se a pessoa tiver 17 anos
+//avisar para voltar quando fizer 18 anos
+if (idade == 17){
+    console.log('volte quando fizer 18 anos')
+}
+
